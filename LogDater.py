7e6682290e3c476470626219a2d08ebd0c3c9034
nathan <nmattick@gmail.com>
2020-05-04 21:45:55 +0100

    Reading datetime from line and calculating the diff.

--- LogDater.py
+++ LogDater.py
@@ -5,6 +5,7 @@ from tkinter import filedialog, messagebox
 root = tk.Tk()
 root.withdraw()
 log_file = 'Logs/asdo-10.177.176.21.log'
+log_file = 'Logs/tmp.log'
 # log_file = filedialog.askopenfilename(#
 #     initialdir = "C:\Logs",
 #     filetypes = (("log files","*.log"),("all files","*.*")))
@@ -15,12 +16,23 @@ print("Reading data from {}".format(log_file))
 
 # Open (in readonly) log_file file for parsing
 import re
-s_nums = 'one1two22three333four'
-print(re.split('\d+', s_nums))
+
+from datetime import datetime
+from datetime import timedelta
+
+time_now = datetime(0)
 
 log_data = open(log_file, 'r')
 for line in log_data:
     print('line: {}'.format(line))
-    l = re.split('2.', line.strip())
-    print("l:    {}".format(l))
+    [the_datetime, rest] = re.split('[\+]', line.strip(), 1)
+    print('the_datetime={}'.format(the_datetime))
+
+    datetime_object = datetime.strptime(the_datetime, '%Y-%m-%dT%H:%M:%S')
+    print('datertime_object={}'.format(datetime_object))
+
+    diff = (datetime_object - time_now).total_seconds()
+    print('diff={}'.format(diff))
+    time_now = datetime_object
+
     input("Press Enter to continue...")
